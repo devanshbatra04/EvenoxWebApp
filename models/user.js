c8879b55751b8bc6d34dc48f13979d5f07334079
devanshbatra04 <33961252+devanshbatra04@users.noreply.github.com>
2018-05-27 00:59:36 +0530

    Revert "Roller"

--- models/user.js
+++ models/user.js
@@ -1,21 +1,16 @@
 var mongoose = require('mongoose');
-
-var passportLocalMongoose = require("passport-local-mongoose");
-
+var passportLocalMongoose = require('passport-local-mongoose');
 
 var userSchema = mongoose.Schema({
     username: String,
     password : String,
-    email: String,
-    name: String,
-    phoneNumber : String
-
+    email : String,
+    phoneNumber : String,
+    name : String
 });
 
 userSchema.plugin(passportLocalMongoose);
 
-
 var User = mongoose.model("User", userSchema);
-
 
 module.exports = User;
