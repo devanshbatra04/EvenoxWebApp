c72cfb6e978a32f06674ec4c963516c3819d2e5c
devanshbatra04 <devanshbatra04@gmail.com>
2018-05-26 20:27:06 +0530

    User Shema associated with mongoose

--- models/user.js
+++ models/user.js
@@ -1,9 +1,12 @@
 var mongoose = require('mongoose');
+var passportLocalMongoose = require('passport-local-mongoose');
 
 var userSchema = mongoose.Schema({
     username: String,
     password : String
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 var User = mongoose.model("User", userSchema);
 
