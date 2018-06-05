82aa594444a848a6c52ee1f03add7d2b327fff97
devanshbatra04 <devanshbatra04@gmail.com>
2018-06-06 00:58:12 +0530

    login page updated to show new frontend

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ app.get("/secret", isLoggedIn, function(req,res){
 });
 
 app.get("/login", function(req,res){
-    res.render('login');
+    res.render('reg-login.ejs');
 });
 
 app.post('/login', passport.authenticate("local", {
