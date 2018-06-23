5c7c4fa91cdecaf8d9ea7baa067908b4c9ab8c0f
devanshbatra04 <devanshbatra04@gmail.com>
2018-06-24 00:40:21 +0530

    bhot silly mistake

--- models/subscriber.js
+++ models/subscriber.js
@@ -1,12 +1,9 @@
 var mongoose = require('mongoose');
-var passportLocalMongoose = require("passport-local-mongoose");
 
 var userSchema = mongoose.Schema({
     email: String,
     name: String,
 });
-
-userSchema.plugin(passportLocalMongoose);
 
 
 var subscriber = mongoose.model("subscriber", userSchema);
